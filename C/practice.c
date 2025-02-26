4f6c82384cce5fdca9c23130b4189293c09ce0db
hayatul-islam <hayatulemon340@gmail.com>
2025-02-26 12:48:36 +0600

    practice

--- C/practice.c
+++ C/practice.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main() {
-    int great = "Hello World"
-    printf(great);
+    int myNum = 15;            
+    float myFloatNum = 5.99;   
+    char myLetter = 'D';       
+    
+    // Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
     return 0;
 }
